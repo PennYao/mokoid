280266f0882d00f913ff050aa5a7fe08d5ad10f5
PennYao <yaosp@sz.net.cn>
2012-10-27 13:38:46 +0800

    Update frameworks/base/service/jni/com_mokoid_server_LedService.cpp

--- frameworks/base/service/jni/com_mokoid_server_LedService.cpp
+++ frameworks/base/service/jni/com_mokoid_server_LedService.cpp
@@ -30,7 +30,7 @@ mokoid_init(JNIEnv *env, jclass clazz)
     return -1;
 }
 
-static jboolean mokoid_setOn(JNIEnv* env, jobject thiz, jint led, jfloat timeout)
+static jboolean mokoid_setOn(JNIEnv* env, jobject thiz, jint led, jfloat timeout, jSting str)
 {
     LOGI("LedService JNI: mokoid_setOn() is invoked.");
 
@@ -38,7 +38,7 @@ static jboolean mokoid_setOn(JNIEnv* env, jobject thiz, jint led, jfloat timeout
         LOGI("LedService JNI: sLedDevice was not fetched correctly.");
         return -1;
     } else {
-        return sLedDevice->set_on(sLedDevice, led);
+        return sLedDevice->set_on(sLedDevice, led, timeout, str);
     }
 
     return 0;
@@ -62,7 +62,7 @@ static jboolean mokoid_setOff(JNIEnv* env, jobject thiz, jint led, jfloat x,
 static const JNINativeMethod gMethods[] = {
     {"_init",	  	"()Z",
 			(void*)mokoid_init},
-    { "_set_on",          "(IF)Z",
+    { "_set_on",          "(IFL java/lang/String;)Z",
                         (void*)mokoid_setOn },
     { "_set_off",          "(I)Z",
                         (void*)mokoid_setOff },
